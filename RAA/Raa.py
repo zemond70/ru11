de8d523e2551dccf9cb74135be8f4074297e6b3d
zemond70 <82406031+zemond70@users.noreply.github.com>
2021-04-12 20:53:07 +0300

    Add files via upload

---
+++ RAA/Raa.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+# Note: This script runs Wifite from within a cloned git repo.
+# The script `bin/raa` is designed to be run after installing (from /usr/sbin), not from the cwd.
+
+from raa import __main__
+__main__.entry_point()
